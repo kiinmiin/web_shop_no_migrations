792f333f60a03dc8a70de5ee7b27bcfd94b1ef57
kiinmiin <rasmo.kiin@voco.ee>
2025-03-07 13:54:23 +0200

    Create products table and manage table data half done

---
+++ controllers/product.js
@@ -0,0 +1,14 @@
+const Product = require('../models/product')
+
+class productController {
+
+    async getAllProducts(req, res) {
+        const products = await Product.findAll()
+        console.log(products)
+        res.status(201).json({
+            products: products
+        })
+    }
+}
+
+module.exports = new productController()
